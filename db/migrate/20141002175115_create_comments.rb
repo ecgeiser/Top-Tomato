727c6534b7df286cfc6e26bae16d95f817a9f353
Elwend <elizabeth.wendling@gmail.com>
2014-10-02 14:40:15 -0400

    Updating relationships

--- db/migrate/20141002175115_create_comments.rb
+++ db/migrate/20141002175115_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :body
-      t.string :movie_id, index: true
-      t.string :user_id, index: true
+      t.references :movie_id, index: true
+      t.references :user_id, index: true
     end
   end
 end
